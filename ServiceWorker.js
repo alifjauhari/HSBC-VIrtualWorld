d40990f9b8389763d99e41f04e85b0a550720201
Alif Kamal Jauhari <alifkamal@student.telkomuniversity.ac.id>
2024-07-15 09:56:49 +0700

    Added game

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Cube Studio-HSBC-Room Builder-0.2";
+const contentToCache = [
+    "Build/Build9.loader.js",
+    "Build/Build9.framework.js.gz",
+    "Build/Build9.data.gz",
+    "Build/Build9.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
